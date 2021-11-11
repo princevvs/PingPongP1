2e2727b3d1e69f6c65bb7caeaac3668dfd4b73a6
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-11-11 19:09:19 +0000

    Added movement to the paddles

--- main.py
+++ main.py
@@ -32,6 +32,34 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+#Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_b_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_up,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_up,"Down")
+
 #Main Game loop
 while True:
     wn.update()
